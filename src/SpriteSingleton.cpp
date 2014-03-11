1b7ba99708d44a692fe9d7068ac2ca6cf9273d0c
Phoenix Perry <contact@phoenixperry.com>
2014-03-11 12:48:28 -0400

    adding in ipad feedback

---
+++ src/SpriteSingleton.cpp
@@ -0,0 +1,9 @@
+//
+//  SpriteSingleton.cpp
+//  OfBookOsc
+//
+//  Created by Phoenix Perry on 3/8/14.
+//
+//
+
+#include "SpriteSingleton.h"
---
+++ src/SpriteSingleton.h
@@ -0,0 +1,14 @@
+//
+//  SpriteSingleton.h
+//  OfBookOsc
+//
+//  Created by Phoenix Perry on 3/8/14.
+//
+//
+
+#ifndef __OfBookOsc__SpriteSingleton__
+#define __OfBookOsc__SpriteSingleton__
+
+#include <iostream>
+
+#endif /* defined(__OfBookOsc__SpriteSingleton__) */
